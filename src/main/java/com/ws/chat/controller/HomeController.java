75d58ac127b68192dd88ca414910d2c87839cc89
kms <kmslkh@naver.com>
2021-07-14 20:16:42 +0900

    [UPDATE] update signup controller, html

--- src/main/java/com/ws/chat/controller/HomeController.java
+++ src/main/java/com/ws/chat/controller/HomeController.java
@@ -1,35 +1,54 @@
 package com.ws.chat.controller;
 
-import lombok.extern.slf4j.Slf4j;
+import com.ws.chat.dto.UserRequest;
+import com.ws.chat.service.UserService;
+import com.ws.chat.utils.UserValidator;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
-@Slf4j
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final UserService userService;
+    private final UserValidator userValidator;
 
     @GetMapping("/")
     public String home(){
-        log.info("home -----------------------------------");
         return "index";
     }
 
     @GetMapping("/login")
     public String login(Model model){
-        log.info("login-----------------------------------");
         return "login";
     }
 
     @GetMapping("/login-error")
     public String loginError(Model model){
         model.addAttribute("loginError", true);
-        log.info("login error-----------------------------------");
         return "login";
     }
 
     @GetMapping("/signup")
-    public String signUp(){
+    public String signUp(Model model){
+        model.addAttribute("userRequest", new UserRequest());
         return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String signup(@Valid UserRequest userRequest, BindingResult bindingResult){
+        userValidator.validate(userRequest, bindingResult);
+        if(bindingResult.hasErrors()){
+            return "signup";
+        }else {
+            userService.save(userRequest);
+            return "redirect:/";
+        }
     }
 }
