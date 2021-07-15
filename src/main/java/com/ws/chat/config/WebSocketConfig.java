29c39a232141b8e976e2de0ca09052e13388cc06
kms <kmslkh@naver.com>
2021-07-16 03:35:36 +0900

    [ADD] WebSocketConfig implements WebSocketMessageBrokerConfigurer

---
+++ src/main/java/com/ws/chat/config/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package com.ws.chat.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/sub"); // subscribe
+        registry.setApplicationDestinationPrefixes("/pub"); // publish
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // Websocket Connect Endpoint setting
+        registry.addEndpoint("/chat").setAllowedOriginPatterns("*").withSockJS();
+    }
+}
