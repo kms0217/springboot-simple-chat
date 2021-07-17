5403c473d46f3fe6121ae5489722a95c955aa374
kms <kmslkh@naver.com>
2021-07-17 18:03:06 +0900

    [UPDATE] User, Chatroom일치하는 ChatroomUser QueryMehtod추가

--- src/main/java/com/ws/chat/repository/ChatroomUserRepository.java
+++ src/main/java/com/ws/chat/repository/ChatroomUserRepository.java
@@ -1,11 +1,15 @@
 package com.ws.chat.repository;
 
+import com.ws.chat.dto.Chatroom;
 import com.ws.chat.dto.ChatroomUser;
 import com.ws.chat.dto.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ChatroomUserRepository extends JpaRepository<ChatroomUser, Long> {
     List<ChatroomUser> findByUser(User user);
+
+    Optional<ChatroomUser> findByUserAndChatroom(User user, Chatroom chatroom);
 }
